ac5c8a7d30d97eb652f04e161057d0219102c9f5
Jonathan Brown <jbrown@acuity.social>
2024-05-14 13:54:10 +0700

    Fix GitHub link.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 zola build
-rsync -avhP --stats --del public/ jbrown@index.acuity.network:index.acuity.network
+rsync -avhP --stats --del public/ index.acuity.network:index.acuity.network
