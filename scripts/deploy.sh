c7dbb05fa272d49f064dbd0096bd957416b1af8c
Jonathan Brown <jbrown@acuity.network>
2025-05-18 15:37:20 +0700

    index.acuity.social => index.acuity.network

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 zola build
-rsync -avhP --stats --del public/ index.acuity.social:index.acuity.network
+rsync -avhP --stats --del public/ index.acuity.network:index.acuity.network
